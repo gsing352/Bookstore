335cbe2b0e5dab396d2fa3910365ca455f4680fb
gsing352 <145233989+gsing352@users.noreply.github.com>
2023-12-08 15:22:00 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
 import { BooklistProvider } from "./context/BookListContext";
 <script src="https://apis.google.com/js/platform.js" async defer></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofGJlTmNWU/mx0s5dL/tSzzp2YfoRSJo4" crossorigin="anonymous"></script>
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
