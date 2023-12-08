8c223e8897bc2f52afbeb24dbeeaf6a641b13af2
gsing352 <145233989+gsing352@users.noreply.github.com>
2023-12-08 15:19:34 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
 import { BooklistProvider } from "./context/BookListContext";
 <script src="https://apis.google.com/js/platform.js" async defer></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofGJlTmNWU/mx0s5dL/tSzzp2YfoRSJo4" crossorigin="anonymous"></script>
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
